81b91f8de576628349440167574713f6632235f5
KushagraGoyal <68071923+KushagraGoyal1601@users.noreply.github.com>
2026-01-03 18:38:44 +0530

    base code setup

---
+++ Stockly/Stockly/StocklyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocklyApp.swift
+//  Stockly
+//
+//  Created by Kushagra Goyal on 03/01/26.
+//
+
+import SwiftUI
+
+@main
+struct StocklyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
